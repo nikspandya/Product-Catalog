66bf904a6c27b8a0dc5ee6243e52981d2ed4f614
nikspandya <nikspandya2@gmail.com>
2023-01-14 10:12:42 +0100

    set up frontend with next, react, typescript, eslint

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import React from 'react';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello World From Niks</h1>
+    </div>
+  );
+};
+
+export default Home;
